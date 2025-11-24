4024658ac4c3d26303e62f5b4f29e66de1dec225
anacatarinapalmeida <uc2022222845@student.uc.pt>
2025-11-24 12:11:45 +0000

    novo js para cores

---
+++ coresIngredientes.js
@@ -0,0 +1,19 @@
+ // array de cores para ingredientes liquidos
+const ingredientColors = {
+    "Gin": "#b9f2ff",
+    "Grand Marnier": "#ffb347",
+    "Lemon juice": "#fff44f",
+    "Grenadine": "#e32636",
+    "Amaretto": "#d2b48c",
+    "Baileys irish cream": "#f5e6c8",
+    "Cognac": "#c68642",
+    "Heavy cream": "#fffdd0",
+    "Milk": "#ffffff",
+    "Egg white": "#f8f8ff",
+    "Orange juice": "#ffa500",
+    "Vodka": "#e1e1e1",
+    "Rum": "#f4e2d8",
+    "Tequila": "#e5e4e2",
+    "Triple sec": "#f7cac9",
+    "Soda water": "#b0e0e6",
+};
